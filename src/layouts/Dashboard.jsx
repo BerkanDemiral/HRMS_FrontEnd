6f60c79a882426c5472384a2c9c271518201e229
Berkan Demiral <alfonso.brkn@gmail.com>
2021-06-19 13:22:09 +0300

    test commit

--- src/layouts/Dashboard.jsx
+++ src/layouts/Dashboard.jsx
@@ -1,5 +1,10 @@
 import React from 'react'
 import {GridColumn, GridRow, Grid } from 'semantic-ui-react'
+import Categories from './Categories'
+import JobPostingList from "../pages/JobPositionList"
+import CandidateList from "../pages/CandidateList"
+import EmployerList from "../pages/EmployerList"
+import { Route } from 'react-router';
 
 export default function Dashboard() {
     return (
@@ -7,9 +12,12 @@ export default function Dashboard() {
             <Grid columns={2} divided>
                 <GridRow>
                     <GridColumn width={4}>
+                        <Categories/>
                     </GridColumn>
                     <GridColumn width={12}>
-                        
+                        <Route exact path="/JobPostings" component={JobPostingList} />
+                        <Route exact path="/Candidates" component={CandidateList} />
+                        <Route exact path="/Employers" component={EmployerList} />
                     </GridColumn>
                 </GridRow>
             </Grid>
