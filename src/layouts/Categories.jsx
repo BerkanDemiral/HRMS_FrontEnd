af11bf801a73196164ab5a52d29f889a99d322d1
Berkan Demiral <alfonso.brkn@gmail.com>
2021-06-19 13:21:58 +0300

    test commit

--- src/layouts/Categories.jsx
+++ src/layouts/Categories.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import { Menu } from 'semantic-ui-react'
+
 export default function Categories() {
-
-        <div> 
-
+return(
+        <div>
+                <Menu pointing vertical size="large" >
+                        <Menu.Item icon="users" name='İş Arayanlar'/>
+                        <Menu.Item icon="bullhorn" name='İş İlanları' />
+                        <Menu.Item icon="globe" name='İşverenler' />
+                </Menu>
         </div>
 
-   
-    
+
+)
 }
