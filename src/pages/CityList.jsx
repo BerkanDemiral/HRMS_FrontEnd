4b515e679a1944b7d967bc86a08e0db790be33d1
Berkan Demiral <alfonso.brkn@gmail.com>
2021-06-22 15:51:07 +0300

    Login panel and some edit

--- src/pages/CityList.jsx
+++ src/pages/CityList.jsx
@@ -1,6 +1,11 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { Link } from "react-router-dom";
+import {Icon , Menu, Table } from 'semantic-ui-react'
 
 export default function CityList() {
+
+    
+
     return (
         <div>
             
